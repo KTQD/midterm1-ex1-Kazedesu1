976c8df43d32b2b9f065f9f83c31f50ea8f5f0ef
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2024-03-14 07:55:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Hehe","Hay","007",10000,1);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Hello","Hay lắm","007",10000,"Hoàng Phong",100,"Hài");;
         book.showInfo();
 
     }
