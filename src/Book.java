71c8efc69bcd6a6132f99b8e967e84cd938224f4
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2024-03-14 07:55:57 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,23 @@
-public class Book {
+public class Book extends Item {
+    private String author;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Tên: " + name);
+        System.out.println("Mô tả: " + super.getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Giá: " + price);
+        System.out.println("Tác giả: " + author);
+        System.out.println("Số trang: " + numberOfPages);
+        System.out.println("Thể loại: " + genre);
+    }
 }
